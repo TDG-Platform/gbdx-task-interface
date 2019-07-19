5e017d7e8e436c85162accb161a75e248d08bff8
Jon Duckworth <jon.duckworth@digitalglobe.com>
2019-07-19 12:30:03 -0600

    More clear failure message on import test

--- test/test_basic_import.py
+++ test/test_basic_import.py
@@ -1,3 +1,5 @@
 def test_import():
-    from gbdx_task_interface import GbdxTaskInterface
-    assert True, 'Could not import package'
+    try:
+        from gbdx_task_interface import GbdxTaskInterface
+    except ImportError:
+        assert False, 'Could not import package'
